54906e8f3277b1207f1d41e575994bdc0e639e70
Wojciech Borawski <70426903+WojBor87@users.noreply.github.com>
2024-08-20 07:59:22 +0200

    Update ChangeDate.ps1
    New version of script

--- ChangeDate.ps1
+++ ChangeDate.ps1
@@ -1,11 +1,20 @@
 # Ustaw bieżącą datę i godzinę
-$currentDate = Get-Date
+$currentDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
 # Formatuj datę i godzinę do użycia w nazwie pliku logu
-$logFileName = "logFile_" + $currentDate.ToString("yyyy-MM-dd_HH-mm-ss") + ".log"
+$logFileName = "LOGI\logFile_ChangeTime.log"
 
-# Start Transkrypcji logów
-Start-Transcript -Path $logFileName
+# Funkcja do zapisywania do logu
+function Write-Log {
+    param (
+        [string]$message
+    )
+    Add-Content -Path $logFileName -Value "$currentDate - $message"
+}
+
+Write_log "====================================="
+Write_log "= Script Run at $currentDate ="
+Write_log "====================================="
 
 try {
 	# Pobierz liste ścieżek do folderów z pliku directoryPaths.txt
@@ -13,15 +22,15 @@ try {
 	
 	#Sprawdzamy wszystkie ścieżki
 	foreach ($directoryPath in $directoryPaths) {
-		"Path: $($directoryPath)"
+		Write-Log "Path: $($directoryPath)"
 		#  Walidacja ścieżki
 		if (-Not (Test-Path -Path $directoryPath -PathType Container)) {
-			"Provided path: $($directoryPath) in directoryPath.txt does not exist or is not a directory."
-
-            # Stop Transkrypcji logów
-			Stop-Transcript
-			exit
+			Write-Log "Provided path: $($directoryPath) in directoryPath.txt does not exist or is not a directory."
+            continue
 		}
+
+        # Licznik zmodyfikowanych plików
+        $modifiedFilesCount = 0
 
 		# Przejście przez wszystkie pliki w folderze i podfolderach oraz ustawienie nowych dat
 		Get-ChildItem -Path $directoryPath -Recurse -File | ForEach-Object {
@@ -30,44 +39,50 @@ try {
 				[System.IO.File]::SetLastAccessTime($_.FullName, $currentDate)
 				[System.IO.File]::SetLastWriteTime($_.FullName, $currentDate)
 				# Wyświetlenie wyniku operacji
-				"File: $($_.FullName) - All dates successfully updated to $currentDate"
+				$modifiedFilesCount++
 			} catch {
 				# Obsługa błędów
-				"Failed to update dates for file: $($_.FullName) - Error: $_"
+				Write-Log "Failed to update dates for file: $($_.FullName) - Error: $_"
 			}
-        	}
+        }
+
+        Write-Log "In directory: $($directoryPath) - $($modifiedFilesCount) files changed"
 	}
-    
-    Copy-Item -Path "directoryPath.txt" -Destination "directoryPathOld.txt"
+	
+    Copy-Item -Path "directoryPath.txt" -Destination "OldDirectoryPath.txt"
     Clear-Content -Path "directoryPath.txt"
 	
-    # Stop Transkrypcji logów
-	Stop-Transcript
+	$subject = 'Zakończono aktualizację dat'
+	$priority = 'Normal'
 
-	$sendMailMessageSplat = @{
-		From = 'test@domena.pl'
-		To = 'user@domena.pl'
-		Subject = 'Zakończono aktualizację dat' 
-		Attachments = $logFileName
-		SmtpServer = 'serwer SMTP'
-	}
- 
 } catch {
 
-    "An error occurred: $_"
-    # Stop Transkrypcji logów
-    Stop-Transcript  
-	
-	$sendMailMessageSplat = @{
-		From = 'test@domena.pl'
-		To = 'user@domena.pl'
-		Subject = 'Błąd w aktualizacji dat' 
-		Body = 'Wystąpił błąd w trakcie przetwarzania dat. W załączeniu log'
-		Attachments = $logFileName
-		Priority = 'High'
-		SmtpServer = 'serwer SMTP'
-	}
+    Write-Log "An error occurred: $_"
+	$subject = 'Błąd w aktualizacji dat'
+	$priority = 'High'
 
 } finally {
-    Send-MailMessage @sendMailMessageSplat
+	
+	Write-Log "Message will be sent with subcject: $subject on priority: $priority"
+	
+    # Logowanie wiadomości o wysłaniu e-maila
+    try {
+        $sendMailMessageSplat = @{
+            From = 'e-mail@domena.com'
+            To = 'e-mail1@domena.com, e-mail2@domena.com'
+            Subject = $subject
+            Attachments = $logFileName
+            SmtpServer = 'smtp server'
+            Priority = $priority
+            Port = 25
+        }
+        
+        Send-MailMessage @sendMailMessageSplat
+        Write-Log "Email sent successfully to recipients."
+
+    } catch {
+        Write-Log "Failed to send email. Error: $_"
+    }
+
+    exit
 }
